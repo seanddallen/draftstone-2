ff02a2f9430c7ff759746ab5587b401cb62ce507
Sean Tayler <seantayler@gAM7acd39.local>
2019-08-20 18:14:21 -0700

    fixed import paths

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import ModesPage from "./components/modes/ModesPage";
-import ProfilePage from "./components/profile/ProfilePage";
-import SettingsPage from "./components/settings/SettingsPage";
-import DraftPage from "./components/draft/DraftPage";
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import ModesPage from "./components/modes/ModesPage.vue";
+import ProfilePage from "./components/profile/ProfilePage.vue";
+import SettingsPage from "./components/settings/SettingsPage.vue";
+import DraftPage from "./components/draft/DraftPage.vue";
 
 
 Vue.use(Router);
